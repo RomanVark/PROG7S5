9a5a43d05299ca3f31db67f709c86129bca8d21d
Romanvark <jrromero@uamv.edu.ni>
2025-04-21 12:44:32 -0600

    Prueba de doble commit

--- Actividad4/ejercicio2.py
+++ Actividad4/ejercicio2.py
@@ -1,3 +1,12 @@
-numero = int(input("Ingrese un número: "))
-if(numero > 10):
-    print("El número es mayor que 10")
+import datetime as dt
+
+
+fecha_inicio_sesion = input("Ultimo inicio de sesion YYYY-MM-DD: ")
+fecha_inicio_sesion = dt.datetime.strptime(fecha_inicio_sesion, "%Y-%m-%d") 
+
+fecha_actual = dt.datetime.now()
+contar_dias = (fecha_actual - fecha_inicio_sesion).days
+if contar_dias > 30:
+    print("Inactivo por mas de 30 dias")
+else:
+    print("Estamos bien.")
